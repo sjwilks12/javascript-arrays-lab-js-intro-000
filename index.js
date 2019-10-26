6a6cb708d09a287b1af531a0777dc1b056fe6eb0
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-26 08:04:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newKittens = kittens[0, kittens.length - 1]
+  var newKittens = kittens.slice(-1)
   return newKittens
 }
 
